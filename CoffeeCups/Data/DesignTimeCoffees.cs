3025322080aa9060d63119d9411e419d5be59649
Nina Vyedin <nina.vyedin@gmail.com>
2016-04-14 19:58:45 -0400

    add the data file (was caught by the gitignore the first time)

---
+++ CoffeeCups/Data/DesignTimeCoffees.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using MvvmHelpers;
+
+namespace CoffeeCups
+{
+	public class DesignTimeCoffees
+	{
+		public static Grouping<string, CupOfCoffee>[] Coffees
+		{
+			get
+			{
+				return new CupOfCoffee[] {
+					new CupOfCoffee { MadeAtHome = true, DateUtc = DateTime.Now },
+					new CupOfCoffee { MadeAtHome = false, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(1))},
+					new CupOfCoffee { MadeAtHome = false, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(2)) },
+					new CupOfCoffee { MadeAtHome = true, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(3)) },
+					new CupOfCoffee { MadeAtHome = false, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(3)) },
+					new CupOfCoffee { MadeAtHome = true, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(2)) },
+					new CupOfCoffee { MadeAtHome = true, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(4)) },
+					new CupOfCoffee { MadeAtHome = false, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(3)) },
+					new CupOfCoffee { MadeAtHome = true, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(1)) }
+				}.GroupBy(t => t.DateUtc.ToString())
+				 .Select(t => new Grouping<string, CupOfCoffee>(t.Key, t))
+				 .ToArray();
+			}
+		}
+	}
+}
