0cfdfdce3397014f2088e581c566e383221e6799
XamDemo <nina.vyedin@xamarin.com>
2016-04-24 15:39:56 -0400

    fix up the layout and design for demo

--- CoffeeCups/Data/DesignTimeCoffees.cs
+++ CoffeeCups/Data/DesignTimeCoffees.cs
@@ -4,23 +4,21 @@ using MvvmHelpers;
 
 namespace CoffeeCups
 {
+	// Mock data to populate the design-time list of coffees
 	public class DesignTimeCoffees
 	{
+		// A collection of fake data to play with at design-time
 		public static Grouping<string, CupOfCoffee>[] Coffees
 		{
 			get
 			{
 				return new CupOfCoffee[] {
-					new CupOfCoffee { MadeAtHome = true, DateUtc = DateTime.Now },
-					new CupOfCoffee { MadeAtHome = false, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(1))},
-					new CupOfCoffee { MadeAtHome = false, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(2)) },
-					new CupOfCoffee { MadeAtHome = true, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(3)) },
-					new CupOfCoffee { MadeAtHome = false, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(3)) },
-					new CupOfCoffee { MadeAtHome = true, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(2)) },
-					new CupOfCoffee { MadeAtHome = true, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(4)) },
-					new CupOfCoffee { MadeAtHome = false, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(3)) },
-					new CupOfCoffee { MadeAtHome = true, DateUtc = DateTime.Now.Subtract (TimeSpan.FromDays(1)) }
-				}.GroupBy(t => t.DateUtc.ToString())
+					new CupOfCoffee { Type = "Hacienda", Notes = "Tangy, citrus", DateUtc = Convert.ToDateTime("04/16/2016 15:50:50"), Image = "home.png"},
+					new CupOfCoffee { Type = "Costa Rica", Notes = "Almonds, toffee", DateUtc = Convert.ToDateTime("04/17/2016 16:10:20"), Image = "out.png"},
+					new CupOfCoffee { Type = "Kona", Notes = "Caramel notes", DateUtc = Convert.ToDateTime("04/20/2016 08:17:20"), Image = "coffee1.jpg"},
+					new CupOfCoffee { Type = "Los Planes", Notes = "Delightful", DateUtc = Convert.ToDateTime("04/21/2016 20:10:25"), Image = "coffee2.jpg"},
+					new CupOfCoffee { Type = "Blue Mountain", Notes = "Chocolatey", DateUtc = Convert.ToDateTime("04/22/2016 10:11:20"), Image = "coffee3.jpg"},
+				}.GroupBy(t => t.DateDisplay)
 				 .Select(t => new Grouping<string, CupOfCoffee>(t.Key, t))
 				 .ToArray();
 			}
--- CoffeeCups/Model/CupOfCoffee.cs
+++ CoffeeCups/Model/CupOfCoffee.cs
@@ -3,51 +3,56 @@ using Humanizer;
 
 namespace CoffeeCups
 {
-    public class CupOfCoffee
-    {
-        /// <summary>
-        /// Gets or sets the user identifier.
-        /// </summary>
-        /// <value>The user identifier.</value>
-        [Newtonsoft.Json.JsonProperty("userId")]
-        public string UserId {get;set;}
+	public class CupOfCoffee
+	{
+		/// <summary>
+		/// Gets or sets the user identifier.
+		/// </summary>
+		/// <value>The user identifier.</value>
+		[Newtonsoft.Json.JsonProperty("userId")]
+		public string UserId { get; set; }
 
-        [Newtonsoft.Json.JsonProperty("Id")]
-        public string Id { get; set; }
+		[Newtonsoft.Json.JsonProperty("Id")]
+		public string Id { get; set; }
 
-        [Microsoft.WindowsAzure.MobileServices.Version]
-        public string AzureVersion { get; set; }
+		[Microsoft.WindowsAzure.MobileServices.Version]
+		public string AzureVersion { get; set; }
 
-        /// <summary>
-        /// Gets or sets the date UTC.
-        /// </summary>
-        /// <value>The date UTC.</value>
-        public DateTime DateUtc { get; set;}
+		/// <summary>
+		/// Gets or sets the date UTC.
+		/// </summary>
+		/// <value>The date UTC.</value>
+		public DateTime DateUtc { get; set; }
 
-        /// <summary>
-        /// Gets or sets a value indicating whether this <see cref="CoffeeCups.CupOfCoffee"/> made at home.
-        /// </summary>
-        /// <value><c>true</c> if made at home; otherwise, <c>false</c>.</value>
-        public bool MadeAtHome{ get; set; }
+		/// <summary>
+		/// Describes the type of coffee.
+		/// </summary>
+		/// <value>type of coffee</value>
+		public string Type { get; set; }
 
-        /// <summary>
-        /// Gets or sets the OS of the user
-        /// </summary>
-        /// <value>The OS</value>
-        public string OS { get; set; }
+		/// <summary>
+		/// Gets or sets the OS of the user
+		/// </summary>
+		/// <value>The OS</value>
+		public string OS { get; set; }
 
+		/// <summary>
+		/// Gets the description
+		/// </summary>
+		/// <value>notes</value>
+		public string Notes { get; set; }
 
-        [Newtonsoft.Json.JsonIgnore]
-        public string DateDisplay => DateUtc.ToLocalTime().ToString("d"); 
+		/// <summary>
+		/// Gets or sets the image path
+		/// </summary>
+		/// <value>Image file path</value>
+		public string Image { get; set; }
 
-        [Newtonsoft.Json.JsonIgnore]
-        public string TimeDisplay => DateUtc.ToLocalTime().ToString("t"); 
+		[Newtonsoft.Json.JsonIgnore]
+		public string TimeDisplay => DateUtc.ToLocalTime().ToString("t");
 
-        [Newtonsoft.Json.JsonIgnore]
-        public string AtHomeDisplay => MadeAtHome ? "Made At Home" : string.Empty;
-
-        [Newtonsoft.Json.JsonIgnore]
-        public string Image => MadeAtHome ? "home.png" : "out.png";
-    }
+		[Newtonsoft.Json.JsonIgnore]
+		public string DateDisplay => $"{DateUtc.DayOfWeek.ToString()}, {DateUtc.ToLocalTime().ToString("d")}";
+	}
 }
 
