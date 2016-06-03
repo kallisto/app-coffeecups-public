7fb45648c98a16ab36706de656375a6079ef4ae2
Nina Vyedin <nina.vyedin@gmail.com>
2016-06-03 08:16:39 -0400

    Add the initial "add coffee page".

---
+++ CoffeeCups/View/AddCoffeePage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace CoffeeCups
+{
+	public partial class AddCoffeePage : ContentPage
+	{
+		public AddCoffeePage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
