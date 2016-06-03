d39facdd28281cfac5b4f937522f249ed9a29891
Nina Vyedin <nina.vyedin@gmail.com>
2016-06-03 08:28:26 -0400

    Initial support for adding coffee and taking photo with media plugin

--- CoffeeCups/ViewModel/CoffeesViewModel.cs
+++ CoffeeCups/ViewModel/CoffeesViewModel.cs
@@ -8,12 +8,18 @@ using FormsToolkit;
 using System.Linq;
 using CoffeeCups.Helpers;
 using Microsoft.WindowsAzure.MobileServices;
+using Plugin.Media;
 
 namespace CoffeeCups
 {
 	public class CoffeesViewModel : BaseViewModel
 	{
 		AzureService azureService;
+		string loadingMessage;
+		ICommand loadCoffeesCommand;
+		ICommand takePhotoCommand;
+		ICommand addCoffeeCommand;
+
 		public CoffeesViewModel()
 		{
 			azureService = new AzureService();
@@ -22,29 +28,44 @@ namespace CoffeeCups
 		public ObservableRangeCollection<CupOfCoffee> Coffees { get; } = new ObservableRangeCollection<CupOfCoffee>();
 		public ObservableRangeCollection<Grouping<string, CupOfCoffee>> CoffeesGrouped { get; } = new ObservableRangeCollection<Grouping<string, CupOfCoffee>>();
 
-		string loadingMessage;
+		public ICommand LoadCoffeesCommand =>
+			loadCoffeesCommand ?? (loadCoffeesCommand = new Command(async () => await ExecuteLoadCoffeesCommandAsync()));
+
+		public ICommand TakePhotoCommand =>
+			takePhotoCommand ?? (takePhotoCommand = new Command(async () => await ExecuteTakePhotoCommandAsync()));
+
+		public ICommand AddCoffeeCommand =>
+			addCoffeeCommand ?? (addCoffeeCommand = new Command(async () => await ExecuteAddCoffeeCommandAsync()));
+
 		public string LoadingMessage
 		{
 			get { return loadingMessage; }
 			set { SetProperty(ref loadingMessage, value); }
 		}
 
-		ICommand loadCoffeesCommand;
-		public ICommand LoadCoffeesCommand =>
-			loadCoffeesCommand ?? (loadCoffeesCommand = new Command(async () => await ExecuteLoadCoffeesCommandAsync()));
+		void SortCoffees()
+		{
+			var groups = from coffee in Coffees
+						 orderby coffee.DateUtc descending
+						 group coffee by coffee.DateDisplay
+				into coffeeGroup
+						 select new Grouping<string, CupOfCoffee>($"{coffeeGroup.Key} ({coffeeGroup.Count()})", coffeeGroup);
+			CoffeesGrouped.ReplaceRange(groups);
+		}
 
 		async Task ExecuteLoadCoffeesCommandAsync()
 		{
 			if (IsBusy)
 				return;
-			
 			try
 			{
-
 				if (!Settings.IsLoggedIn)
 				{
+					// log in user
 					await azureService.Initialize();
-					var user = await DependencyService.Get<IAuthentication>().LoginAsync(azureService.MobileService, MobileServiceAuthenticationProvider.MicrosoftAccount);
+					var user = await DependencyService.Get<IAuthentication>().LoginAsync(
+						azureService.MobileService,
+						MobileServiceAuthenticationProvider.MicrosoftAccount);
 					if (user == null)
 						return;
 				}
@@ -72,19 +93,52 @@ namespace CoffeeCups
 			}
 		}
 
-		void SortCoffees()
+		async Task ExecuteTakePhotoCommandAsync()
 		{
-			var groups = from coffee in Coffees
-						 orderby coffee.DateUtc descending
-						 group coffee by coffee.DateDisplay
-				into coffeeGroup
-						 select new Grouping<string, CupOfCoffee>($"{coffeeGroup.Key} ({coffeeGroup.Count()})", coffeeGroup);
-			CoffeesGrouped.ReplaceRange(groups);
-		}
+			if (IsBusy)
+				return;
+			try
+			{
+				// take a photo and 
+				await CrossMedia.Current.Initialize();
 
-		ICommand addCoffeeCommand;
-		public ICommand AddCoffeeCommand =>
-			addCoffeeCommand ?? (addCoffeeCommand = new Command(async () => await ExecuteAddCoffeeCommandAsync()));
+				if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+				{
+					MessagingService.Current.SendMessage<MessagingServiceAlert>("message", new MessagingServiceAlert
+					{
+						Cancel = "OK",
+						Message = "No camera available",
+						Title = "Camera"
+					});
+					return;
+				}
+
+				var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+				{
+					Directory = "Sample",
+					Name = "test.jpg"
+				});
+
+				if (file == null)
+					return;
+
+				MessagingService.Current.SendMessage<MessagingServiceAlert>("message", new MessagingServiceAlert
+				{
+					Cancel = "OK",
+					Message = $"{file.Path}",
+					Title = "Location"
+				});
+
+			}
+			catch (Exception ex)
+			{
+				Debug.WriteLine("OH NO!" + ex);
+			}
+			finally
+			{
+				IsBusy = false;
+			}
+		}
 
 		async Task ExecuteAddCoffeeCommandAsync()
 		{
@@ -94,6 +148,7 @@ namespace CoffeeCups
 			{
 				if (!Settings.IsLoggedIn)
 				{
+					// log user in
 					await azureService.Initialize();
 					var user = await DependencyService.Get<IAuthentication>().LoginAsync(azureService.MobileService, MobileServiceAuthenticationProvider.MicrosoftAccount);
 					if (user == null)
@@ -102,6 +157,7 @@ namespace CoffeeCups
 					LoadingMessage = "Adding Coffee...";
 					IsBusy = true;
 
+					// load coffees from Azure
 					var coffees = await azureService.GetCoffees();
 					Coffees.ReplaceRange(coffees);
 
