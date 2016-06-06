3dd7f9849579a63b05ce1be4260892d6f5347cf1
Nina Vyedin <nina.vyedin@gmail.com>
2016-06-06 12:27:20 -0400

    remove unused usings

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿using Foundation;
 using UIKit;
-using FormsToolkit;
-using Xamarin.Forms;
 using ImageCircle.Forms.Plugin.iOS;
 
 namespace CoffeeCups.iOS
