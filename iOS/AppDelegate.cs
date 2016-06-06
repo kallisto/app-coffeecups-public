125de5c50b51b16be5f08ffb224b40f0041f2e71
Nina Vyedin <nina.vyedin@gmail.com>
2016-06-06 16:52:18 -0400

    [portable] formatting

--- iOS/AppDelegate.cs
+++ iOS/AppDelegate.cs
@@ -4,21 +4,21 @@ using ImageCircle.Forms.Plugin.iOS;
 
 namespace CoffeeCups.iOS
 {
-    [Register("AppDelegate")]
-    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
-    {
-        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
-        {
+	[Register("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+		public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+		{
 			global::Xamarin.Forms.Forms.Init();
-            ImageCircleRenderer.Initialize();
+			ImageCircleRenderer.Initialize();
 
-            LoadApplication(new App());
+			LoadApplication(new App());
 
-            Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
-            SQLitePCL.CurrentPlatform.Init();
-            FormsToolkit.iOS.Toolkit.Init();
-            return base.FinishedLaunching(app, options);
-        }
-    }
+			Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
+			SQLitePCL.CurrentPlatform.Init();
+			FormsToolkit.iOS.Toolkit.Init();
+			return base.FinishedLaunching(app, options);
+		}
+	}
 }
 
